085c23b6f60efabd88d5c2509919be50755865d1
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-05-01 14:50:42 +0900

    add CodingTest

---
+++ CodingTest/src/sub2/Solution.java
@@ -0,0 +1,19 @@
+package sub2;
+
+import java.util.Scanner;
+
+/**
+ * 날짜 : 2026/05/01
+ * 이름 : 남현호
+ * 내용 : 프로그래머스 기출문제 2번 각도 합치기 
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int angle1 = sc.nextInt();
+        int angle2 = sc.nextInt();
+
+        int sum_angle = (angle1 + angle2) % 360;
+        System.out.println(sum_angle);
+    }
+}
