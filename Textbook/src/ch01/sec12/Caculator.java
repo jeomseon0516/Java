e296f6ded27d3e204e9f0e2e97886288209eee85
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-05-01 15:00:42 +0900

    add Textbook

---
+++ Textbook/src/ch01/sec12/Caculator.java
@@ -0,0 +1,10 @@
+package ch01.sec12;
+
+public class Caculator {
+	public static void main(String[] args) {
+		int x = 1;					// 변수 x를 선언하고 1로 초기화
+		int y = 2;					// 변수 y를 선언하고 2로 초기화
+		int result = x + y;			// 변수 result를 선언하고 x와 y를 더한 값으로 초기화
+		System.out.println(result); // 콘솔에 출력하는 메서드 호출
+	}
+}
