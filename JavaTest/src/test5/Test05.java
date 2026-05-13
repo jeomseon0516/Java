ed74be50bf922a68e7e75364df895acc82b063ac
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-05-13 17:51:35 +0900

    adddd

---
+++ JavaTest/src/test5/Test05.java
@@ -0,0 +1,87 @@
+package test5;
+
+class Apple {
+	private String country;
+	private int price;
+	
+	public Apple(String country, int price) {
+		this.country = country;
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return new StringBuilder("Apple [country=")
+			.append(country)
+			.append(", price=")
+			.append(price)
+			.append("]")
+			.toString();
+	}
+}
+
+class Banana {
+	private String country;
+	private int price;
+	
+	public Banana(String country, int price) {
+		this.country = country;
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return new StringBuilder("Apple [country=")
+			.append(country)
+			.append(", price=")
+			.append(price)
+			.append("]")
+			.toString();
+	}
+}
+
+class Grape {
+	private String country;
+	private int price;
+	
+	public Grape(String country, int price) {
+		this.country = country;
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return new StringBuilder("Apple [country=")
+			.append(country)
+			.append(", price=")
+			.append(price)
+			.append("]")
+			.toString();
+	}
+}
+
+public class Test05 {
+	public static void main(String[] args) {
+		Apple apple = new Apple("한국", 3000);
+		Banana banana = new Banana("미국", 2000);
+		Grape grape = new Grape("일본", 1000);
+		
+		showInfo(apple);
+		showInfo(banana);
+		showInfo(grape);
+	}
+	
+	public static void showInfo(Object fruit) {
+		
+		if(fruit instanceof Apple) {
+			Apple apple = (Apple)fruit;
+			System.out.println(apple);
+		}else if(fruit instanceof Banana) {
+			Banana banana = (Banana)fruit;
+			System.out.println(banana);
+		}else if(fruit instanceof Grape) {
+			Grape grape = (Grape)fruit;
+			System.out.println(grape);
+		}
+	}
+}
---
+++ my_module_b/src/pack3/C.java
@@ -0,0 +1,8 @@
+package pack3;
+
+public class C {
+	//메서드 선언
+	public void method() {
+		System.out.println("C-method 실행");
+	}
+}
