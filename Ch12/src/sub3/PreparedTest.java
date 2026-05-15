b268bd818aa5523d44d7a7850d8fd3bf727a22ee
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-05-15 11:34:41 +0900

    modified Ch12

--- Ch12/src/sub3/PreparedTest.java
+++ Ch12/src/sub3/PreparedTest.java
@@ -1,5 +1,6 @@
 package sub3;
 
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,7 +12,7 @@ public class PreparedTest {
 	public static void main(String[] args) {
 		
 		System.out.println("데이터베이스 매니지먼트 프로그램");
-		System.out.println("작업 선택하세요.(1:입력, 2:조회, 3:수정, 4:삭제)");
+		System.out.println("작업 선택하세요.(1:입력, 2:전체조회, 3:검색, 4:수정, 5: 삭제)");
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -50,17 +51,73 @@ public class PreparedTest {
 					User1DAO.insert(dto);
 					//System.out.println(dto);
 					
+					break;
+				case 2:
+					System.out.println("전체 조회 작업...");
+					List<User1DTO> dtoList = User1DAO.select();
 					
-					
+					for (User1DTO user1 : dtoList) {
+						System.out.println("-----------------");
+						System.out.print(user1.getUserid() + ", ");
+						System.out.print(user1.getName() + ", ");
+						System.out.print(user1.getHp() + ", ");
+						System.out.print(user1.getAge() + "\n");
+						System.out.println("-----------------");
+					}
 					break;
 				case 3:
-					System.out.println("조회 작업...");
+					System.out.println("검색 작업...");
+					System.out.println("검색할 이름 입력 : ");
+					String searchName = sc.next();
+					
+					List<User1DTO> searchedList = User1DAO.search(searchName);
+					System.out.println("검색결과---------------------------------");
+					for (User1DTO user1 : searchedList) {
+						System.out.println("-----------------");
+						System.out.print(user1.getUserid() + ", ");
+						System.out.print(user1.getName() + ", ");
+						System.out.print(user1.getHp() + ", ");
+						System.out.print(user1.getAge() + "\n");
+						System.out.println("-----------------");
+					}
+					
 					break;
 				case 4:
 					System.out.println("수정 작업...");
+					System.out.print("수정할 아이디 입력 : ");
+					String updateUserid = sc.next();
+					
+					System.out.println("수정 작업...");
+					System.out.print("수정할 이름 입력 : ");
+					String updateName = sc.next();
+					
+					System.out.println("수정 작업...");
+					System.out.print("수정할 휴대폰 입력 : ");
+					String updateHp = sc.next();
+					
+					System.out.println("수정 작업...");
+					System.out.print("수정할 나이 입력 : ");
+					int updateAge = sc.nextInt();
+					
+					User1DTO updateDto = new User1DTO();
+					updateDto.setUserid(updateUserid);
+					updateDto.setName(updateName);
+					updateDto.setHp(updateHp);
+					updateDto.setAge(updateAge);
+					
+					User1DAO.update(updateDto);
+					
+					System.out.println("수정 완료");
 					break;
 				case 5:
 					System.out.println("삭제 작업...");
+					
+					System.out.print("삭제할 아이디 입력 : ");
+					String deleteUserid = sc.next();
+					
+					User1DAO.delete(deleteUserid);
+					
+					System.out.println("삭제완료");
 					break;
 			}
 		}
